8b5f152321409418bf561414f443dde5737a5e69
ashishkumar822GA <ashish@generalautonomy.in>
2024-11-08 09:28:26 +0530

    update

---
+++ Examples/ROS/ORB_SLAM2/build/gtest/CMakeFiles/gtest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gtest.dir/src/gtest-all.cc.o"
+  "/home/isl-server/ashish/libraries/orb_slam2/ORB_SLAM2-master/Examples/ROS/ORB_SLAM2/lib/libgtest.pdb"
+  "/home/isl-server/ashish/libraries/orb_slam2/ORB_SLAM2-master/Examples/ROS/ORB_SLAM2/lib/libgtest.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gtest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Examples/ROS/ORB_SLAM2/build/gtest/CMakeFiles/gtest_main.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gtest_main.dir/src/gtest_main.cc.o"
+  "/home/isl-server/ashish/libraries/orb_slam2/ORB_SLAM2-master/Examples/ROS/ORB_SLAM2/lib/libgtest_main.pdb"
+  "/home/isl-server/ashish/libraries/orb_slam2/ORB_SLAM2-master/Examples/ROS/ORB_SLAM2/lib/libgtest_main.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gtest_main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
